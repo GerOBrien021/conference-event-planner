66ec2bdada99304e8638376b211085f86273a391
GerOBrien021 <gerard.obrien.cork@gmail.com>
2025-01-21 07:18:56 -0500

    Added code for Add-Ons (aka av)

--- src/ConferenceEvent.jsx
+++ src/ConferenceEvent.jsx
@@ -178,7 +178,7 @@ const ConferenceEvent = () => {
                                         </div>
                                     ))}
                                 </div>
-                                <div className="total_cost">Total Cost: {avTotalCost}</div>
+                                <div className="total_cost">Total Cost: ${avTotalCost}</div>
 
                             </div>
 
